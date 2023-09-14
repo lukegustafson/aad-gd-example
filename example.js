5b411462c6b78e8cb7456a16d31a4cb3bc82b94e
Luke Gustafson <lukeg3@gmail.com>
2023-09-14 06:01:15 -0400

    Fix comment

--- example.js
+++ example.js
@@ -304,7 +304,7 @@ function objective_function(heights)
 	//Run AAD
 	aad_calc_derivs();
 	
-	//Return the total error and the derivative wrt the polynomial coefficents
+	//Return the total error and the gradient
 	return [total_length.value, heights_aad.map(c => c.derivative)];
 }
 
